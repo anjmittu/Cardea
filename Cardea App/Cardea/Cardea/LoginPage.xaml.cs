8bf176c3120f2735b498dde590e06f0fa54d403b
Nick Mittu <nick@mittudev.com>
2018-09-22 17:05:00 -0400

    Update dir names

---
+++ Cardea App/Cardea/Cardea/LoginPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Fitbit_Data_Analysis_App
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            WebView.Source = "https://www.fitbit.com";
+        }
+
+        void Login(object sender, System.EventArgs e)
+        {
+            WebView.IsVisible = true;
+            LoginButton.IsVisible = false;
+            LoginLabel.IsVisible = false;
+        }
+
+        void webOnEndNavigating(object sender, WebNavigatedEventArgs e)
+        {
+            
+        }
+    }
+}
