d3d4ff8aeeeef5179e0348650bcbfdb420f5a976
Nick Mittu <nick@mittudev.com>
2018-09-22 23:25:16 -0400

    Add login

--- Cardea/Cardea/Cardea.Android/MainActivity.cs
+++ Cardea/Cardea/Cardea.Android/MainActivity.cs
@@ -10,7 +10,12 @@ using Android.OS;
 namespace Cardea.Droid
 {
 	[Activity (Label = "Cardea", Icon = "@drawable/icon", Theme="@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    [IntentFilter(new[] { Android.Content.Intent.ActionView },
+        DataScheme = "cardea",
+        DataHost = "auth",
+        Categories = new[] { Android.Content.Intent.CategoryDefault, Android.Content.Intent.CategoryBrowsable },
+        AutoVerify=true)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
 	{
 		protected override void OnCreate (Bundle bundle)
 		{
@@ -19,7 +24,9 @@ namespace Cardea.Droid
 
 			base.OnCreate (bundle);
 
-			global::Xamarin.Forms.Forms.Init (this, bundle);
+            var data = Intent?.Data?.EncodedAuthority;
+
+            global::Xamarin.Forms.Forms.Init (this, bundle);
 			LoadApplication (new Cardea.App ());
 		}
 	}
--- Cardea/Cardea/Cardea/LoginPage.xaml.cs
+++ Cardea/Cardea/Cardea/LoginPage.xaml.cs
@@ -1,30 +1,73 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Newtonsoft.Json;
 
 namespace Cardea
 {
+    class AuthResp
+    {
+        public string access_token { get; set; }
+        public string user_id { get; set; }
+    }
+
 	public partial class LoginPage : ContentPage
 	{
 		public LoginPage()
 		{
 			InitializeComponent();
-            WebView.Source = "https://www.fitbit.com";
+            WebView.Source = "https://www.fitbit.com/oauth2/authorize?client_id=22D7B9&response_type=code&scope=activity+heartrate+nutrition+sleep+weight&redirect_uri=cardea://auth";
         }
 
         void Login(object sender, System.EventArgs e)
         {
             WebView.IsVisible = true;
-            LoginButton.IsVisible = false;
-            //LoginLabel.IsVisible = false;
+            LoginForm.IsVisible = false;
         }
 
-        void webOnEndNavigating(object sender, WebNavigatedEventArgs e)
+        void webOnNavigating(object sender, WebNavigatingEventArgs e)
         {
+            LoginButton.IsEnabled = false;
+        }
 
+            void webOnEndNavigating(object sender, WebNavigatedEventArgs e)
+        {
+            LoginButton.IsEnabled = true;
+            if (e.Url.StartsWith("cardea://auth"))
+            {
+                WebView.IsVisible = false;
+                LoginForm.IsVisible = true;
+
+                var URL = new Uri(e.Url);
+                string code = e.Url.Split(new string[] { "code=" }, System.StringSplitOptions.RemoveEmptyEntries)[1].Split('#')[0];
+
+                Dictionary<string, string> form = new Dictionary<string, string>();
+                form["code"] = code;
+                form["grant_type"] = "authorization_code";
+                form["client_id"] = "22D7B9";
+                form["redirect_uri"] = "cardea://auth";
+
+                var content = new FormUrlEncodedContent(form);
+
+                var client = new HttpClient();
+                var authheader = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.ASCII.GetBytes("22D7B9:c501ba0677ae034ac4d64051695b1ffa")));
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.ASCII.GetBytes("22D7B9:c501ba0677ae034ac4d64051695b1ffa")));
+                var result = client.PostAsync("https://api.fitbit.com/oauth2/token", content).Result;
+
+
+                string resp = result.Content.ReadAsStringAsync().Result;
+                var tokens = JsonConvert.DeserializeObject<AuthResp>(resp);
+                App.auth_token = tokens.access_token;
+                App.userid = tokens.user_id;
+
+                Navigation.PushModalAsync(new HomePage());
+            }
         }
     }
 }
